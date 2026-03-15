922e8d5b3ef00300bbfd66b786126f96c9e0c740
Soraia34 <soraiap037@gmail.com>
2026-03-15 00:22:51 +0000

    Lição 4.2
    Lição 4.2 feita!

---
+++ Assets/Course Library/Scripts/Spawn Manager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefabs;
+    private float spawnRange = 9;
+    void Start()
+    {
+       Instantiate(enemyPrefabs, GenerateSpawnPosition(), enemyPrefabs.transform.rotation);
+    }
+
+    
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+}
